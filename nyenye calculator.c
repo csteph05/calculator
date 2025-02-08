75d4b296e4eba0c1602e8f99e00df17d70e03e3c
csteph05 <charlescabrera697@gmail.com>
2025-02-08 15:11:47 +0800

    add simple calculator

---
+++ nyenye calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main ()
+{
+	char op;
+	float num1, num2;
+	float result;
+	
+	printf("Enter an Operation: ");
+	scanf("%c", &op);
+	printf("Enter the first value: ");
+	scanf("%f", &num1);
+	printf("Enter the second value: ");
+	scanf("%f", &num2);
+	
+
+	{
+	switch(op)
+	{
+		case '+': 
+			result = num1 + num2;
+			break;
+		case '-':
+			result = num1 - num2;
+			break;
+		case '/':
+			result = num1 / num2;
+			 break;
+		case 'x':
+			result = num1 * num2;
+			break;
+	}
+		}
+	printf("The result of %f and %f is %f", num1, num2, result);
+	
+	return 0;
+}
